1142e2183cd08bdf0439f8d5b1a1704ac836c66c
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-12-13 15:09:54 -0600

    nuevos modelos

--- Chicago/Documentos/models.py
+++ Chicago/Documentos/models.py
@@ -10,36 +10,14 @@ class Usuario(AbstractUser):
     def __str__(self):
         return self.username
 
-'''
-class Usuario(models.Model):
-    usuario = models.OneToOneField(User, on_delete = models.CASCADE)
-    imagen_perfil = models.ImageField(validators=[FileExtensionValidator(["jpg", "png"])], upload_to="imagenesPerfil", default="")
-    """Model definition for Usuario."""
-
-    class Meta:
-        """Meta definition for Usuario."""
-
-        verbose_name = 'Usuario'
-        verbose_name_plural = 'Usuarios'
-'''
-
-class Repositorio(models.Model):
-    nombre = models.CharField(max_length=100)
-    idUsuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-
-    class Meta:
-        """Meta definition for Repositorio"""
-
-        verbose_name = 'Repositorio'
-        verbose_name_plural = 'Repositorios'
-
 class Documento(models.Model):
     nombreDoc = models.CharField(max_length=100)
     fechaSubida = models.DateField(auto_now_add=True)
     fechaLimite = models.DateField()
-    idRepositorio = models.ForeignKey(Repositorio, on_delete=models.CASCADE)
     documento = models.FileField(upload_to="documentos", validators=[
                                  FileExtensionValidator(["docx", "pdf"])])
+    imagen_documento = models.ImageField(
+        validators=[FileExtensionValidator(["jpg", "png"])], upload_to="imagenesDocumento")
 
     class Meta:
         verbose_name = 'Documento'
@@ -49,10 +27,28 @@ class Permiso(models.Model):
     idUsuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     idDocumento = models.ForeignKey(Documento, on_delete = models.CASCADE)
     firmado = models.BooleanField()
+    esPropietario = models.BooleanField()
 
     class Meta:
         verbose_name = 'Permiso'
         verbose_name_plural = 'Permisos'
+
+class Notificacion(models.Model):
+    idUsuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    idDocumento = models.ForeignKey(Documento, on_delete = models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Notificacion'
+        verbose_name_plural = 'Notificaciones'
+
+class Chat(models.Model):
+    idUsuarioRemitente = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    idUsuarioDestinatario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    mensaje = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Chat'
+        verbose_name_plural = 'Chats'
 
 
 
